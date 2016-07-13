0cc4fb613dd04edb151873df96306888442d9200
Raymond Gonzalez <raymondgon7@gmail.com>
2016-07-13 09:39:36 -0400

    Create session.js

---
+++ code/js/session.js
@@ -0,0 +1,19 @@
+function debateSessionBegin(currentTimer,currentRound){
+	      this.currentRound = currentRound;
+	      
+	      if(getTotalTimer() === 0 ){
+	      	    Closing();
+	      	    chatActiveTil(); /// chat timer is armed  allowing users to speak for 10 minutes after;
+	      	          // gives warning one minutes before session shuts down
+	      }
+	      // start timerStep
+	      // open voice for user 1
+	      // close voice for user one
+	      // if timer = 0
+	      // Intermission
+	      // if timer = 0
+	      // open voice for user 2
+	      // close voice for user 2
+	      // end session()
+	      
+}
